830dd0dbf806d65c3500a894fe57bcfa80cb4002
AshuMItter <84823182+AshuMItter@users.noreply.github.com>
2025-06-14 23:28:56 +0530

    Update settings.py

--- DjangoWebProject1/settings.py
+++ DjangoWebProject1/settings.py
@@ -13,6 +13,30 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import posixpath
 
+# For Azure deployment
+if 'WEBSITE_HOSTNAME' in os.environ:
+    DEBUG = False
+    ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+    # Configure database using Azure environment variables
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ['DBNAME'],
+            'HOST': os.environ['DBHOST'],
+            'USER': os.environ['DBUSER'],
+            'PASSWORD': os.environ['DBPASS'],
+        }
+    }
+else:
+    DEBUG = True
+    ALLOWED_HOSTS = []
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
