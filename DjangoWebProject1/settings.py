05046ef387281112347d7e591641bbd2f854ebf1
AshuMItter <84823182+AshuMItter@users.noreply.github.com>
2025-06-15 03:13:18 +0530

    Update settings.py

--- DjangoWebProject1/settings.py
+++ DjangoWebProject1/settings.py
@@ -80,8 +80,12 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware', 
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'DjangoWebProject1.urls'
 
